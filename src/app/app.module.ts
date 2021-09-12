2a1e01d89b345caad336b544f4ecb226908e5cc7
Hygor Martins <hygor.martins74@gmail.com>
2021-09-12 20:19:03 -0300

    Criando template Angular pagina Home e Pulls

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from 'src/pages/home/home.component';
+import { HeaderComponent } from 'src/common/header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule
